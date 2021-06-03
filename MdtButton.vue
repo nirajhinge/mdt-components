22c32fa6e18d0c56ae2022e06bc2fc09b4971593
nirajhinge <54309996+nirajhinge@users.noreply.github.com>
2021-06-03 14:34:36 +0100

    Initial commit

---
+++ MdtButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<button>
+		<mdt-label></mdt-label>
+	</button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import MdtLabel from "./MdtLabel";
+
+export default defineComponent({
+	name: "MdtButton",
+	components: {MdtLabel}
+});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ MdtLabel.vue
@@ -0,0 +1,18 @@
+<template>
+	<label>
+		Button Label Two
+	</label>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "MdtLabel"
+});
+
+</script>
+
+<style scoped>
+
+</style>
